8264bf51a1511adde8efd04298977db9c628909d
Christian Fenger <fengercs@gmail.com>
2025-09-19 20:03:56 -0500

    Implemented contructor into Character.cs

--- Models/Character.cs
+++ Models/Character.cs
@@ -2,20 +2,20 @@ using System.Text.Json.Serialization;
 
 namespace W4_assignment_template.Models;
 
-public class Character
+public class Character(string name, string @class, int level, int hp, List<string> equipment)
 {
     [JsonPropertyName("name")]
-    public string Name { get; set; }
+    public string Name { get; set; } = name;
 
     [JsonPropertyName("class")]
-    public string Class { get; set; }
+    public string Class { get; set; } = @class;
 
     [JsonPropertyName("level")]
-    public int Level { get; set; }
+    public int Level { get; set; } = level;
 
     [JsonPropertyName("hp")]
-    public int HP { get; set; }
+    public int HP { get; set; } = hp;
 
     [JsonPropertyName("equipment")]
-    public List<string> Equipment { get; set; }
+    public List<string> Equipment { get; set; } = equipment ?? new List<string>();
 }
